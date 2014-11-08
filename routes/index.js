fef80b03fbc695dc11afaad18b50299dd7264126
SunLn <dengchenhua@gmail.com>
2014-11-08 17:13:25 +0800

    bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: '双十一搞基不寂寞'
+    });
 });
 
 module.exports = router;
